e24e6d7c541028141e8834ca351237940e33710a
K2\urmanicr <robert.urmanic@k2.cz>
2025-12-26 22:29:44 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-<h2>Add a quiz</h2>
+<h2>quiz</h2>
   all random X amount <br>
   all random from specific chapter <br>
   all random from all chapter, X questions per chapter <br>
 Add a hint (a b c question choices) <br>
 Add a post quiz log with all questions and answers <br>
-<h2>add an answer to question</h2>
+<h3>so we have to add also answers to the questions</h3>
+<h3>we have to make the actual quiz - make the program ask quesitons</h3>
+<h2>START THINKING ABOUT MAKING THIS PROGRAM A DESKTOP APPLICATION</h2>
+<h2>START THINKING ABOUT MAKING THIS PROGRAM RUNNABLE ON A WEB</h2>
+
--- src/main/java/org/example/Application/Application.java
+++ src/main/java/org/example/Application/Application.java
@@ -11,60 +11,65 @@ public class Application {
     private static int chapterId = 0;
     private static int subChapterId = 0;
     private static ResultSet rs;
+    private static boolean run = true;
 
     public static void run() {
         connection = DatabaseConnection.connect("jdbc:sqlserver://URMANICR-DH4241:1433;databaseName=Quiz;encrypt=false", "testDB", "testDB");
 
-        System.out.println("What do you want to do?");
-        System.out.println("----------------------------------");
-        System.out.println("1. Add a chapter -----------------");
-        System.out.println("2. Add a subchapter --------------");
-        System.out.println("3. Add a question ----------------");
-        System.out.println("4. See all chapter questions -----");
+        while (run) {
 
-        Scanner input = new Scanner(System.in);
-        // žádná kontrola na input, budu to používat jen já...
-        try {
-            switch (Integer.parseInt(input.nextLine())) {
-                case 1:
-                    System.out.println("Enter the chapter name:");
-                    insert(input, "INSERT INTO chapter(name) VALUES(?)", 0, 0);
-                    break;
-                case 2:
-                    chapterId = listItemsToChoose("chapter", rs, input);
-                    System.out.println("Type subchapter name:");
-                    if (chapterId == 0)
-                        insert(input, "INSERT INTO subchapter(name) VALUES(?)", 0, 0);
-                    else {
-                        insert(input, "INSERT INTO subchapter(name, chapterId) VALUES(?, ?)", chapterId, 0);
-                    }
-                    break;
-                case 3:
-                    System.out.println("Enter the chapter name:");
-                    chapterId = listItemsToChoose("chapter", rs, input);
-                    System.out.println("Enter the subchapter name:");
-                    subChapterId = listItemsToChoose("subchapter", rs, input);
-                    System.out.println("Enter the question:");
-                    insert(input, "INSERT INTO question(question, chapterId, subchapterId) VALUES(?,?,?)", chapterId, subChapterId);
-                    break;
-                case 4:
-                    Statement selectAll = connection.createStatement();
-                    rs = selectAll.executeQuery("SELECT * FROM Chapter AS CH WITH(nolock)"
-                                                + " INNER JOIN Subchapter AS SU WITH(nolock)"
-                                                + " ON CH.Id = SU.ChapterId"
-                                                + " INNER JOIN question AS QU"
-                                                + " ON QU.ChapterId = CH.Id AND QU.SubchapterId = SU.Id");
-                    while(rs.next()){
-                        System.out.println(rs.getString("question"));
-                    }
-                    break;
-                default:
-                    System.out.println("Nothing found");
-                    break;
+            System.out.println("What do you want to do?");
+            System.out.println("----------------------------------");
+            System.out.println("1. Add a chapter -----------------");
+            System.out.println("2. Add a subchapter --------------");
+            System.out.println("3. Add a question ----------------");
+            System.out.println("4. See all chapter questions -----");
+            System.out.println("9. EXIT");
+
+            Scanner input = new Scanner(System.in);
+            // žádná kontrola na input, budu to používat jen já...
+            try {
+                switch (Integer.parseInt(input.nextLine())) {
+                    case 1:
+                        System.out.println("Enter the chapter name:");
+                        insert(input, "INSERT INTO chapter(name) VALUES(?)", 0, 0);
+                        break;
+                    case 2:
+                        chapterId = listItemsToChoose("chapter", rs, input);
+                        System.out.println("Type subchapter name:");
+                        if (chapterId == 0)
+                            insert(input, "INSERT INTO subchapter(name) VALUES(?)", 0, 0);
+                        else {
+                            insert(input, "INSERT INTO subchapter(name, chapterId) VALUES(?, ?)", chapterId, 0);
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Enter the chapter name:");
+                        chapterId = listItemsToChoose("chapter", rs, input);
+                        System.out.println("Enter the subchapter name:");
+                        subChapterId = listItemsToChoose("subchapter", rs, input);
+                        System.out.println("Enter the question:");
+                        insert(input, "INSERT INTO question(question, chapterId, subchapterId) VALUES(?,?,?)", chapterId, subChapterId);
+                        break;
+                    case 4:
+                        Statement selectAll = connection.createStatement();
+                        rs = selectAll.executeQuery("SELECT CH.name as chname, SU.name as suname, * FROM Chapter AS CH WITH(nolock)"
+                                + " INNER JOIN Subchapter AS SU WITH(nolock)"
+                                + " ON CH.Id = SU.ChapterId"
+                                + " INNER JOIN question AS QU"
+                                + " ON QU.ChapterId = CH.Id AND QU.SubchapterId = SU.Id");
+                        while (rs.next()) {
+                            System.out.println("Chapter: " + rs.getString("chname") + "\n\tSubchapter: " + rs.getString("suname") + "\n\t\tQuestion: " + rs.getString("question"));
+                        }
+                        break;
+                    default:
+                        run = false;
+                        break;
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
     }
 
